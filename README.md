4cbf2a4d8fa4989e731c12da044564c9d4b259c2
LuthGom <lucianomendes23@yahoo.com>
2023-02-08 12:47:00 -0300

    README.md branch main

---
+++ README.md
@@ -0,0 +1,3 @@
+Repositório de Aulas do Módulo de Reactjs - Turma RD
+
+Cada aula uma branch: aula - [numero da aula]
--- aula 1/index.js
+++
@@ -1,5 +0,0 @@
-
-const api = fetch("https://pokeapi.co/api/v2/pokemon/12")
-  .then((result) => result.json())
-  .then((data) => console.log(data))
-  // .catch((erro) => console.log(erro));
