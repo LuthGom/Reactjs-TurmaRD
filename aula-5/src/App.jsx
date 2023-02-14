404140a3f593f04906e82277c3a1978af2f55f01
LuthGom <lucianomendes23@yahoo.com>
2023-02-13 23:57:31 -0300

    aula 5 - a por comentários

--- aula-5/src/App.jsx
+++ aula-5/src/App.jsx
@@ -3,15 +3,16 @@ import Create from "./componentes/Create";
 import Delete from "./componentes/Delete";
 import Read from "./componentes/Read";
 import Update from "./componentes/Update";
-
+import {BrouserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       {/* para visualizar cada componente com mais clareza, sempre deixe apenas um descomentado e os outros 3 comentados. */}
-      {/* <Read />
-      <Update /> */}
-      {/* <Create /> */}
-      {/* <Delete /> */}
+      
+      <Read />
+      <Update /> 
+      <Create />
+      {/* {/* <Delete /> */}
       <Create />
     </div>
   );
